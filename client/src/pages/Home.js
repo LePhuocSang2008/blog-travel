0601c9d8afeb4fa72a60d4a3358ae9116770e560
LePhuocSang2008 <phuocsang5s147@gmail.com>
2022-06-01 16:06:34 +0700

    Initial commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBTypography } from "mdb-react-ui-kit";
+import { useDispatch, useSelector } from "react-redux";
+import { getTours, setCurrentPage } from "../redux/features/tourSlice";
+import CardTour from "../components/CardTour";
+import Spinner from "../components/Spinner";
+import Pagination from "../components/Pagination";
+import { useLocation } from "react-router-dom";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+const Home = () => {
+  const { tours, loading, currentPage, numberOfPages } = useSelector(
+    (state) => ({
+      ...state.tour,
+    })
+  );
+  const dispatch = useDispatch();
+  const query = useQuery();
+  const searchQuery = query.get("searchQuery");
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(getTours(currentPage));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+  return (
+    <MDBContainer className="pt-5"
+    >
+      <MDBRow className="mt-5">
+        {tours.length === 0 && location.pathname === "/" && (
+          <MDBTypography className="text-center mb-0" tag="h2">
+            No Tours Found
+          </MDBTypography>
+        )}
+
+        {tours.length === 0 && location.pathname !== "/" && (
+          <MDBTypography className="text-center mb-0" tag="h2">
+            We couldn't find any matches for "{searchQuery}"
+          </MDBTypography>
+        )}
+
+       
+          <MDBContainer>
+            <MDBRow className="g-2">
+              {tours &&
+                tours.map((item) => <CardTour key={item._id} {...item} />)}
+            </MDBRow>
+          </MDBContainer>
+
+      </MDBRow>
+      {tours.length > 0 && !searchQuery && (
+        <Pagination
+          setCurrentPage={setCurrentPage}
+          numberOfPages={numberOfPages}
+          currentPage={currentPage}
+          dispatch={dispatch}
+        />
+      )}
+    </MDBContainer>
+  );
+};
+
+export default Home;
